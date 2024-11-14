8ebccd85320088f8ed663bd7f225ea0711b9b50e
Matio004 <mateuszkupis@interia.eu>
2024-11-14 23:25:46 +0100

    ints

--- ip_calc.py
+++ ip_calc.py
@@ -2,105 +2,80 @@ from typing import Union
 
 
 class Octet:
-    def __init__(self, octet: Union[int, list], base: int = 10):
-        self.__octet = octet
-        self.__base = base
+    __octet: int
 
-        self.__max10 = 255
+    def __init__(self, octet):
+        if isinstance(octet, int):
+            self.__octet = octet
+        else:
+            self.__octet = octet.__octet
 
         self.check_for_errors()
 
     def __repr__(self):
-        return str(self.__octet) if self.__base == 10 else ''.join(map(str, self.__octet))
+        return f'Octet({self.__octet})'
 
-    @property
-    def octet(self):
-        return self.__octet
-
-    @property
-    def base(self):
-        return self.__base
+    def __str__(self):
+        return f'{self.__octet}'
 
     def check_for_errors(self):
-        self.to_decimal()
-
-        if self.__octet > self.__max10:
+        if self.__octet > 255:
             raise ValueError('Octet should be a decimal number 0-255')
 
-    def to_binary(self):
-        if self.__base != 2:
-            self.__octet = list(map(int, bin(self.__octet)[2:]))
-            self.__octet = [0] * (8 - len(self.__octet)) + self.__octet
-            self.__base = 2
-
-            return self
-
-    def to_decimal(self):
-        if self.__base != 10:
-            self.__octet = int(''.join(map(str, self.__octet)), self.__base)
-            self.__base = 10
-
-            return self
-
     def __add__(self, other):
-        self.to_decimal()
-
-        return Octet(self.__octet+other.octet)
+        return Octet(self.__octet+other.__octet)
 
     def __sub__(self, other):
-        self.to_decimal()
-
-        return Octet(self.__octet-other.octet)
+        return Octet(self.__octet-other.__octet)
 
     def __invert__(self):
-        self.to_binary()
-
-        new = []
-
-        for bit in range(len(self.__octet)):
-            new.append(int(not int(self.__octet[bit])))
-        self.to_decimal()
-
-        return Octet(new, 2)
+        return Octet(~self.__octet & ((1 << 8) - 1))
 
     def __and__(self, other):
-        self.to_decimal()
-
-        return Octet(self.__octet & other.octet)
+        return Octet(self.__octet & other.__octet)
 
     def __len__(self):
-        self.to_binary()
-        temp = len(self.__octet)
-        self.to_decimal()
-        return temp
+        return 8
 
     def count(self, x: int):
-        self.to_binary()
+        temp = self.__octet
+        count = 0
 
-        x = self.__octet.count(x)
-
-        self.to_decimal()
-        return x
+        while temp:
+            count += temp & 1
+            temp >>= 1
+        return count if x else 8 - count
 
 
 class IPv4:
-    def __init__(self, ipv4: str, base: int = 10):
-        self.__ipv4 = [Octet(int(i) if base == 10 else list(map(int, list(i))), base) for i in ipv4.split('.')]
-        self.__base = base
+    def __init__(self, ip: Union[list[int], list[Octet], tuple[int], tuple[Octet]]):
+        self.__ipv4 = [Octet(i) for i in ip]
 
         self.check_for_errors()
 
     @classmethod
     def from_cidr(cls, cidr: int):
         if cidr <= 32:
-            iterable = '1' * cidr + '0' * (32 - cidr)
-            instance = cls('.'.join([iterable[i:i + 8] for i in range(0, len(iterable), 8)]), 2)
-            return instance.to_decimal()
+            temp = 0
+            for _ in range(cidr):
+                temp <<= 1
+                temp |= 1
+            temp <<= 32 - cidr
+            return cls.from_int(temp)
         raise ValueError("Cidr can't be higher than 32")
 
-    @property
-    def ipv4(self):
-        return self.__ipv4
+    @classmethod
+    def from_int(cls, ip: int):
+        temp = [0] * 4
+
+        for i in range(3, -1, -1):
+            temp[i] = ip & 255
+            ip >>= 8
+        return cls(temp)
+
+    @classmethod
+    def from_string(cls, ip: str):
+        return cls(list(map(int, ip.split('.'))))
 
     def __repr__(self):
         return '.'.join(map(str, self.__ipv4))
@@ -111,42 +86,30 @@ class IPv4:
         if len(self.__ipv4) != 4:
             raise ValueError('IPv4 contains 4 octets. Octet value should be 0-255')
 
-    def to_binary(self):
-        for octet in self.__ipv4:
-            if octet.base != 2:
-                octet.to_binary()
-        return self
-
-    def to_decimal(self):
-        for octet in self.__ipv4:
-            if octet.base != 10:
-                octet.to_decimal()
-        return self
-
     def __add__(self, other):
         new = []
         for octet in range(len(self.__ipv4)):
-            new.append(str(self.__ipv4[octet] + other.ipv4[octet]))
-        return IPv4('.'.join(new))
+            new.append(self.__ipv4[octet] + other.__ipv4[octet])
+        return IPv4(new)
 
     def __sub__(self, other):
         new = []
         for octet in range(len(self.__ipv4)):
-            new.append(str(self.__ipv4[octet] - other.ipv4[octet]))
-        return IPv4('.'.join(new))
+            new.append(self.__ipv4[octet] - other.__ipv4[octet])
+        return IPv4(new)
 
     def __invert__(self):
         new = []
 
         for octet in range(len(self.__ipv4)):
-            new.append(str(~self.__ipv4[octet]))
-        return IPv4('.'.join(new))
+            new.append(~self.__ipv4[octet])
+        return IPv4(new)
 
     def __and__(self, other):
         new = []
         for octet in range(len(self.__ipv4)):
-            new.append(str(self.__ipv4[octet] & other.ipv4[octet]))
-        return IPv4('.'.join(new))
+            new.append(self.__ipv4[octet] & other.__ipv4[octet])
+        return IPv4(new)
 
     def count(self, x: int):
         count_ = 0
@@ -159,10 +122,10 @@ class Network:
     def __init__(self, ip: str, mask: str = None):
         if mask is None:
             self.__mask = IPv4.from_cidr(int(ip.split('/')[-1]))
-            self.ip = IPv4(ip.split('/')[0])
+            self.ip = IPv4.from_string(ip.split('/')[0])
         else:
-            self.ip = IPv4(ip)
-            self.__mask = IPv4(mask)
+            self.ip = IPv4.from_string(ip)
+            self.__mask = IPv4.from_string(mask)
 
     def __repr__(self):
         return f'IP: {self.ip}\nMask: {self.__mask}'
@@ -177,11 +140,11 @@ class Network:
 
     @property
     def host0(self):
-        return self.network_address + IPv4('0.0.0.1')
+        return self.network_address + IPv4.from_int(1)
 
     @property
     def host_1(self):
-        return self.broadcast_address - IPv4('0.0.0.1')
+        return self.broadcast_address - IPv4.from_int(1)
 
     @property
     def max_hosts(self):
@@ -199,7 +162,8 @@ if __name__ == '__main__':
                   f'Adres ostatniego hosta: {net.host_1}\n'
                   f'Liczba hostów do zaadresowania: {net.max_hosts}\n')
 
-        except ValueError:
+        except ValueError as ex:
+            print(ex)
             print('Wprowadź dane ponownie...')
 
         except KeyboardInterrupt:
